0842b72ed5b07f1d82431bccb0bc5407b5bc7e72
rmp77-glitch <rmp77@cam.ac.uk>
2026-03-03 02:40:52 +0000

    task 2e, change before markup

--- Task2E.py
+++ Task2E.py
@@ -3,6 +3,9 @@ from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.plot import plot_water_levels
 import datetime 
+import matplotlib.pyplot as plt
+
+
 stations = build_station_list()
 update_water_levels(stations)
 
@@ -14,3 +17,5 @@ for i in range(num):
     station = twoc[i][0]
     dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
     plot_water_levels(station, dates, levels)
+    plt.savefig(f"station_{i}.png")  
+    plt.close()     
