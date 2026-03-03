5ea56a76236696854c57c204a67668002047fd9a
rmp77-glitch <rmp77@cam.ac.uk>
2026-03-03 02:44:04 +0000

    task2E, double check plot 1

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -6,5 +6,20 @@ from datetime import datetime, timedelta
 def plot_water_levels(station, dates, levels):
     """displays a plot of the water level data against time for a station, includes lines for the typical low and high levels"""
 
+    # plots
     plt.plot(dates,levels)
+
+    #adds labels and title
+    plt.title(station.name)
+    plt.xlabel("date")
+    plt.ylabel("water level")
+
+    #subplot lines 
+    plt.axhline(y=station.typical_range[0], color='green', linestyle='--', label="Typical Low")
+    plt.axhline(y=station.typical_range[1], color='red',   linestyle='--', label="Typical High")
+
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+
     plt.show()
